5c0861418b6679dd7102df3d3a7fc9e93f9e2433
ravi <ravilamkoti@gmail.com>
2019-08-21 19:41:26 +0530

    v0.5

--- hackathon/main/views.py
+++ hackathon/main/views.py
@@ -18,6 +18,7 @@ class CompanyProblemsList(ListView):
 
 def StudentFormView(request):
     form = StudentForm()
+    messages.info(request,'Select 4 different problem statements')
     if request.method=='POST':
         form = StudentForm(request.POST)
         if form.is_valid():
@@ -27,8 +28,8 @@ def StudentFormView(request):
             instance.problem_statement_3 = form.cleaned_data['problem_statement_3']
             instance.problem_statement_4 = form.cleaned_data['problem_statement_4']
             email_address = form.cleaned_data['team_leader_email']
-            print(email_address,form.cleaned_data['team_leader_name'])
             send_mail('email/confirmation.tpl',{'team_leader':form.cleaned_data['team_leader_name']},'vcet.hackathon@vcet.edu.in',[email_address,])
+            messages.success(request,'You Have Successfully filled form. We have sent you one email about further details.')
             instance.save()
             return redirect('index')
     return render(request,'main/form.html',{'form':form})
